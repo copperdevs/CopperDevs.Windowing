51ddf60f37198939916d3fe6d6a91838040b63e8
copperdevs <github@copperdevs.com>
2025-04-07 11:26:25 -0500

    extensions comment cleanup

--- src/CopperDevs.Windowing/Extensions.cs
+++ src/CopperDevs.Windowing/Extensions.cs
@@ -14,7 +14,11 @@ public static class Extensions
     /// <param name="value">Color to create from</param>
     /// <returns>The created vector4</returns>
     public static Vector4 ToVector4(this Color value) => new(value.R, value.G, value.B, value.A);
-    
+
+    /// <summary>
+    /// Rounds both values to the nearest amount of digits
+    /// </summary>
+    /// <returns>Both numbers rounded to the numbers nearest to them that contains a number of fractional digits equal to digits.</returns>
     public static Vector2 Round(this Vector2 vector, int digits)
     {
         return new Vector2(MathF.Round(vector.X, digits), MathF.Round(vector.Y, digits));
