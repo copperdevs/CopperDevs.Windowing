0cfdd24dfe2b173bb3d6c6c2dfa1ab2b6a393fb3
copperdevs <github@copperdevs.com>
2026-05-21 02:25:29 -0500

    projects

---
+++ src/raylib/CopperDevs.Windowing.Raylib_cs.Example/Program.cs
@@ -0,0 +1,11 @@
+﻿using CopperDevs.Celesium;
+
+namespace CopperDevs.Windowing.Raylib_cs.Example;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Log.Info("Hello, World!");
+    }
+}
